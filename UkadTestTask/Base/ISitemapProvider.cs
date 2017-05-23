8c7d6153d86ba3830035d8fd06bc22cd2370e055
Tyner <intheheartbeat0@gmail.com>
2017-05-23 10:36:55 +0300

    Implemented ISitemapProvider, declared Sitemap

---
+++ UkadTestTask/Base/ISitemapProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkadTestTask.Base
+{
+    interface ISitemapProvider
+    {
+        Sitemap GetSitemapFromUrl(string url);
+    }
+}
---
+++ UkadTestTask/Base/Sitemap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UkadTestTask.Base
+{
+    public class Sitemap
+    {
+    }
+}
