552c5997e8a366884eb02620ea10e9acff658b8e
Tyner <intheheartbeat0@gmail.com>
2017-05-24 20:26:23 +0300

    Created scanner

---
+++ UkadTestTask/Scanning/Scanner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace UkadTestTask.Scanning
+{
+    public class Scanner : IRegisteredObject
+    {               
+        private bool Cancelled { get; set; }
+
+        public Scanner()
+        {
+            _storagePath = storagePath;
+            Cancelled = false;
+        }
+
+        public void Stop(bool immediate)
+        {
+            
+        }
+    }
+}
