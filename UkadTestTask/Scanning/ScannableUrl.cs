c67d2ed138b4071e73765ad6ca34c0314aa7b43f
Tyner <intheheartbeat0@gmail.com>
2017-05-24 20:29:15 +0300

    Implemented ScannableUrl

---
+++ UkadTestTask/Scanning/ScannableUrl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UkadTestTask.Scanning
+{
+    public class ScannableUrl
+    {
+        public string Url { get; set; }
+        public int LoadTimeMs { get; set; }
+        public int Ping { get; set; }
+        public bool Completed { get; set; }
+    }
+}
